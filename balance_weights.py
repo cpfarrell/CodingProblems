525c6bc762226d17ace7b2046f2340d46f69cc85
cpfarrell <farrell.chris1@gmail.com>
2014-02-22 13:04:03 -0800

    Clean up

--- balance_weights.py
+++ balance_weights.py
@@ -1,11 +1,13 @@
-def equal(weights):
-    known = set([5])
+def can_balance(weights, initial):
+    #Might be possible with an array instead of set for better memory
+    past_differences = set([initial])
     for weight in weights:
-        if weight in known:
+        if weight in past_differences:
             return True
         else:
-            for key in list(known):
-                known.add(abs(weight - key))
+            for old_diff in list(past_differences):
+                past_differences.add(weight - old_diff)
+                past_differences.add(weight + old_diff)
     return False
 
-print equal([2,4,1])
+print can_balance([2,4,8,10,19], 5)
